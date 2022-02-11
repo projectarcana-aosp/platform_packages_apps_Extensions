75a69d858042b7ab96299a4de378fc05d6ca66c2
dlwlrma123 <alexfinhart@gmail.com>
2022-02-11 09:24:42 +0800

    Tresdin: cleanup statusbaricons logic
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- src/org/aospextended/extensions/fragments/StatusbarIcons.java
+++ src/org/aospextended/extensions/fragments/StatusbarIcons.java
@@ -81,8 +81,6 @@ public class StatusbarIcons extends SettingsPreferenceFragment {
     {
         overlayMap.put("com.android.settings", "android.theme.customization.icon_pack.settings");
         overlayMap.put("com.android.systemui", "android.theme.customization.icon_pack.systemui");
-        overlayMap.put("com.android.launcher3", "android.theme.customization.icon_pack.launcher");
-        overlayMap.put("com.android.wallpaper", "android.theme.customization.icon_pack.themepicker");
     }
 
     @Override
