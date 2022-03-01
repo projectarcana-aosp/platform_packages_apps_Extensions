8164c1a09b456002febe44aa681f78f70922591c
SuperDroidBond <superdroidbond@yahoo.com>
2022-03-02 05:16:24 +0800

    Tresdin: Xtended Statusbar Clock Customization [2/2]
    Add Clock size, color & font style.
    
    Port to Pie + Added Color for Clocks - SuperDroidBond
    Clock fonts and size - @temasek
    Port to Oreo by @varund7726
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- src/org/aospextended/extensions/fragments/ClockDateSettings.java
+++ src/org/aospextended/extensions/fragments/ClockDateSettings.java
@@ -52,6 +52,8 @@ import com.android.settings.Utils;
 import com.android.settingslib.search.Indexable;
 
 import com.android.internal.util.aospextended.AEXUtils;
+import org.aospextended.support.preference.CustomSeekBarPreference;
+import org.aospextended.support.colorpicker.ColorPickerPreference;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -70,10 +72,14 @@ public class ClockDateSettings extends SettingsPreferenceFragment
     private static final String PREF_CLOCK_DATE_POSITION = "statusbar_clock_date_position";
     private static final String PREF_CLOCK_SECONDS = "statusbar_clock_seconds";
     private static final String PREF_CLOCK_STYLE = "statusbar_clock_style";
+    private static final String STATUS_BAR_CLOCK_COLOR = "status_bar_clock_color";
+    private static final String STATUS_BAR_CLOCK_SIZE  = "status_bar_clock_size";
+    private static final String STATUS_BAR_CLOCK_FONT_STYLE  = "status_bar_clock_font_style";
 
     public static final int CLOCK_DATE_STYLE_LOWERCASE = 1;
     public static final int CLOCK_DATE_STYLE_UPPERCASE = 2;
     private static final int CUSTOM_CLOCK_DATE_FORMAT_INDEX = 18;
+    static final int DEFAULT_STATUS_CLOCK_COLOR = 0xffffffff;
 
     private ListPreference mClockAmPmStyle;
     private ListPreference mClockDateDisplay;
@@ -83,12 +89,21 @@ public class ClockDateSettings extends SettingsPreferenceFragment
     private ListPreference mStatusBarClockStyle;
     private SwitchPreference mStatusBarClock;
     private SwitchPreference mStatusBarSecondsShow;
+    private ColorPickerPreference mClockColor;
+    private CustomSeekBarPreference mClockSize;
+    private ListPreference mClockFontStyle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.clock_date_settings);
 
+        PreferenceScreen prefSet = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+        
+        int intColor;
+        String hexColor;
+        
         mClockAmPmStyle = (ListPreference) findPreference(PREF_AM_PM_STYLE);
         mClockAmPmStyle.setOnPreferenceChangeListener(this);
         mClockAmPmStyle.setValue(Integer.toString(Settings.System.getInt(getActivity()
@@ -151,6 +166,27 @@ public class ClockDateSettings extends SettingsPreferenceFragment
         mStatusBarClockStyle.setSummary(mStatusBarClockStyle.getEntry());
         mStatusBarClockStyle.setOnPreferenceChangeListener(this);
 
+
+        mClockColor = (ColorPickerPreference) findPreference(STATUS_BAR_CLOCK_COLOR);
+            mClockColor.setOnPreferenceChangeListener(this);
+            intColor = Settings.System.getInt(resolver,
+                    Settings.System.STATUS_BAR_CLOCK_COLOR, DEFAULT_STATUS_CLOCK_COLOR);
+            hexColor = String.format("#%08x", (0xffffffff & intColor));
+            mClockColor.setSummary(hexColor);
+            mClockColor.setNewPreviewColor(intColor);
+
+        mClockSize = (CustomSeekBarPreference) findPreference(STATUS_BAR_CLOCK_SIZE);
+        int clockSize = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_CLOCK_SIZE, 14);
+        mClockSize.setValue(clockSize / 1);
+        mClockSize.setOnPreferenceChangeListener(this);
+
+        mClockFontStyle = (ListPreference) findPreference(STATUS_BAR_CLOCK_FONT_STYLE);
+        int showClockFont = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_CLOCK_FONT_STYLE, 0);
+        mClockFontStyle.setValue(String.valueOf(showClockFont));
+        mClockFontStyle.setOnPreferenceChangeListener(this);
+        
         mClockDatePosition = (ListPreference) findPreference(PREF_CLOCK_DATE_POSITION);
         mClockDatePosition.setOnPreferenceChangeListener(this);
         mClockDatePosition.setValue(Integer.toString(Settings.System.getInt(getActivity()
@@ -193,6 +229,7 @@ public class ClockDateSettings extends SettingsPreferenceFragment
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         AlertDialog dialog;
+ 	ContentResolver resolver = getActivity().getContentResolver();
 
         if (preference == mClockAmPmStyle) {
             int val = Integer.parseInt((String) newValue);
@@ -216,6 +253,26 @@ public class ClockDateSettings extends SettingsPreferenceFragment
                 mClockDateFormat.setEnabled(true);
                 mClockDatePosition.setEnabled(true);
             }
+            return true;
+        } else if (preference == mClockColor) {
+                String hex = ColorPickerPreference.convertToARGB(
+                        Integer.valueOf(String.valueOf(newValue)));
+                preference.setSummary(hex);
+                int intHex = ColorPickerPreference.convertToColorInt(hex);
+                Settings.System.putInt(resolver,
+                        Settings.System.STATUS_BAR_CLOCK_COLOR, intHex);
+                return true;
+        }  else if (preference == mClockSize) {
+            int width = ((Integer)newValue).intValue();
+            Settings.System.putInt(resolver,
+                    Settings.System.STATUS_BAR_CLOCK_SIZE, width);
+            return true;
+        }  else if (preference == mClockFontStyle) {
+            int showClockFont = Integer.valueOf((String) newValue);
+            int index = mClockFontStyle.findIndexOfValue((String) newValue);
+            Settings.System.putInt(resolver, Settings.System.
+                STATUS_BAR_CLOCK_FONT_STYLE, showClockFont);
+            mClockFontStyle.setSummary(mClockFontStyle.getEntries()[index]);
             return true;
         } else if (preference == mClockDateStyle) {
             int val = Integer.parseInt((String) newValue);
